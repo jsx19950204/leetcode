eb54fec4005ad009de815ad17435cf943793e158
贾世雄 <871216371@qq.com>
2019-11-22 12:45:24 +0800

    '第三题'

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -10,8 +10,20 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var len = s.length;
-    return s;
+    var start = 0;
+    var end;
+    var max = 0;
+    var res = '';
+    for (var i = 0, len = s.length; i < len; i++) {
+        var index = res.indexOf(s[i]);
+        res += s[i];
+        if (index !== -1) {
+            max = Math.max(max, res.length - 1);
+            res = res.slice(index + 1, i + 1);
+            start = index + 1;
+        }
+    }
+    return Math.max(max, res.length);
 };
 // @lc code=end
-
+console.log(lengthOfLongestSubstring('ggububgvfk'))
